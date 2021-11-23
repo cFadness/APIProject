caefa3fffbf6ee49842bbf55b2a8ab8b359d5ad4
cFadness <codie1989@gmail.com>
2021-11-23 15:06:44 -0800

    Add all CRUD route handlers.

--- routes/products.js
+++ routes/products.js
@@ -2,12 +2,35 @@ const { Product, validate } = require('../models/product');
 const express = require('express');
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+    try {
+        const products = await Product.find();
+        return res.send(products);
+    } catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        if(!product)
+            return res.status(400).send(`The product with id "${req.params.id}" does
+            not exist.`);
+
+            return res.send(product);
+
+    } catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
 router.post('/', async (req, res) => {
     try {
         const { error } = validate(req.body);
         if (error)
             return res.status(400).send(error);
-            
+
         const product = new Product({
             name: req.body.name,
             description: req.body.description,
@@ -16,6 +39,49 @@ router.post('/', async (req, res) => {
         });
 
         await product.save();
+
+        return res.send(product);
+
+    } catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        const {error} = validate(req.body);
+        if (error) return res.status(400).send(error);
+
+        const product = await Product.findByIdAndUpdate(req.params.id, 
+            {
+                name: req.body.name,
+                description: req.body.description,
+                category: req.body.category,
+                price: req.body.price,
+            },
+            { new: true }
+            );
+
+            if (!product)
+                return res.status(400).send(`The product with id "${req.params.id}"
+                does not exist.`);
+
+            await product.save();
+
+            return res.send(product);
+    } catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+
+        const product = await Product.findByIdAndRemove(req.params.id);
+
+        if (!products)
+            return res.status(400).send(`The product with id "${req.params.id}" does
+            not exist.`);
 
         return res.send(product);
 
